be5d5cfa9812bd6fc113cacc4d9a3dcc7801eca8
Sklyvan <joangraciataribo@gmail.com>
2021-10-03 17:57:36 +0200

    Added timezone detection.

--- Imports.py
+++ Imports.py
@@ -1,6 +1,7 @@
 # # # Python Required Libraries & Modules # # #
 from NetworkRequests import Request, HTML_LocalFile
 from bs4 import BeautifulSoup as BS
+from tzlocal import get_localzone
 import requests, json, sys
 from ScheduleBlocks import *
 from Configuration import *
@@ -11,3 +12,4 @@ CONFIG_FILE = "UserPreferences.ini"
 URL = 'https://gestioacademica.upf.edu/pds/consultaPublica/look%5Bconpub%5DInicioPubHora?entradaPublica=true' # URL principal para establecer una conexión y obtener una sesión.
 URL_RND = 'https://gestioacademica.upf.edu/pds/consultaPublica/look[conpub]MostrarPubHora' # Con esta URL obtenemos un número "aleatorio" que nos permite simular la conexión de un usuario consultando el horario.
 URL_JSON = 'https://gestioacademica.upf.edu/pds/consultaPublica/[Ajax]selecionarRangoHorarios' # Con esta URL obtenermos el archivo JSON que contiene el horario.
+TIMEZONE = get_localzone().key
