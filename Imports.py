63510b6fffe7a004a7f700482581c5b850ed17d0
Sklyvan <37016233+Sklyvan@users.noreply.github.com>
2021-10-26 13:39:08 +0200

    Delete Imports.py

--- Imports.py
+++
@@ -1,8 +0,0 @@
-# # # Python Required Libraries & Modules # # #
-from NetworkRequests import Request, HTML_LocalFile
-from bs4 import BeautifulSoup as BS
-import requests, json, sys, os
-from ScheduleBlocks import *
-from Configuration import *
-from GoogleCalendar import Calendar
-from Constants import *
