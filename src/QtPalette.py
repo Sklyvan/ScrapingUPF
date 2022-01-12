fd454e97261e8ed4a9e045ab5044b3bfb4732e8f
Sklyvan <joangraciataribo@gmail.com>
2022-01-12 19:38:48 +0100

    The dark mode is now nordic theme.

--- src/QtPalette.py
+++ src/QtPalette.py
@@ -1,15 +1,20 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPalette, QColor
 
+Color1 = QColor(46, 52, 64)
+Color2 = QColor(59, 66, 82)
+Color3 = QColor(67, 76, 94)
+Color4 = QColor(76, 86, 106)
+
 darkPalette = QPalette()
-darkPalette.setColor(QPalette.Window, QColor(48, 54, 67)) # Application background
+darkPalette.setColor(QPalette.Window, Color2) # Application background
 darkPalette.setColor(QPalette.WindowText, Qt.white) # Application text
-darkPalette.setColor(QPalette.Base, QColor(37, 42, 52)) # Text box background
+darkPalette.setColor(QPalette.Base, Color4) # Text box background
 darkPalette.setColor(QPalette.AlternateBase, QColor(53, 53, 53)) # I don't know what this is
 darkPalette.setColor(QPalette.ToolTipBase, Qt.white) # Tooltip background
 darkPalette.setColor(QPalette.ToolTipText, Qt.white) # Tooltip text
 darkPalette.setColor(QPalette.Text, Qt.white) # User input text
-darkPalette.setColor(QPalette.Button, QColor(42, 47, 58))
+darkPalette.setColor(QPalette.Button, Color1)
 darkPalette.setColor(QPalette.ButtonText, Qt.white)
 darkPalette.setColor(QPalette.BrightText, Qt.red)
 darkPalette.setColor(QPalette.Link, QColor(42, 130, 218))
